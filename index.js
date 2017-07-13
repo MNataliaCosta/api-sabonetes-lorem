f1b23e3e1afaf6d53e7b184d5bc5597fed518820
Paulo Costa <paulocosta@MacBook-Pro-de-Paulo.local>
2017-07-13 18:28:39 -0300

    API chat

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var expressMongoDb = require('express-mongo-db');
-var resultadosController = require('./controllers/resultados.js')
+
+var resultadosController = require('./controllers/resultados.js');
+var mensagensController = require('./controllers/mensagens.js')
 
 // inicializa o express
 var app = express();
@@ -10,13 +12,8 @@ var app = express();
 app.use(bodyParser.json());
 
 // inicializa mongo e expoe para o express
-app.use(expressMongoDb('mongodb://localhost:27017/lorem'));
+app.use(expressMongoDb('mongodb://172.16.15.143:27017/lorem'));
 
-
-// inicializa o servidor na porta especificada
-app.listen(3000, function() {
-  console.log('Acesse o servidor http://localhost:3000');
-});
 
 //libera o acesso a API a patir de  qualquer cliente, host, etc....
 app.use(function(req, res, next) {
@@ -25,9 +22,16 @@ app.use(function(req, res, next) {
   next();
 });
 
-// importa controllers
-var resultadosController = require('./controllers/resultados.js');
 
-// cria enpoints para funcoes de controllers
+// endpoints formulario
 app.get('/resultados', resultadosController.listar);
 app.post('/resultados', resultadosController.procurar);
+// endpoints mensagens
+app.get('/chat', mensagensController.listar);
+app.post('/chat', mensagensController.criar);
+
+
+// inicializa o servidor na porta especificada
+app.listen(3000, "0.0.0.0", function() {
+  console.log('Acesse o servidor http://172.16.15.143:3000');
+});
